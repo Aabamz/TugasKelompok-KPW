ecc35d8d8762fd0ae4381309ab27bd020b2baf22
sivamustika245 <sivamustika20@gmail.com>
2026-08-23 18:04:35 +0700

    konfigurasi menu sidebar adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -299,101 +299,42 @@ return [
     */
 
     'menu' => [
-        // Navbar items:
+        // Topbar / Navbar Atas
         [
-            'type' => 'navbar-search',
-            'text' => 'search',
-            'topnav_right' => true,
-        ],
-        [
-            'type' => 'fullscreen-widget',
+            'type'         => 'fullscreen-widget',
             'topnav_right' => true,
         ],
 
-        // Sidebar items:
+        // Menu Utama
+        ['header' => 'MAIN MENU'],
         [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
+            'text' => 'Dashboard',
+            'url'  => 'dashboard', // Dibuat oleh Zabran / Siva
+            'icon' => 'fas fa-fw fa-tachometer-alt',
         ],
+
+        // Pengaturan Akun
+        ['header' => 'AKUN SAYA'],
         [
-            'text' => 'blog',
-            'url' => 'admin/blog',
-            'can' => 'manage-blog',
-        ],
-        [
-            'text' => 'pages',
-            'url' => 'admin/pages',
-            'icon' => 'far fa-fw fa-file',
-            'label' => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url' => 'admin/settings',
+            'text' => 'Profil Saya',
+            'url'  => 'profile', // Dibuat oleh Aira
             'icon' => 'fas fa-fw fa-user',
         ],
+
+        // Autentikasi (Akan disembunyikan jika sudah sistem login/register penuh)
+        ['header' => 'AUTENTIKASI'],
         [
-            'text' => 'change_password',
-            'url' => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'text' => 'Login',
+            'url'  => 'login', // Dibuat oleh Evan
+            'icon' => 'fas fa-fw fa-sign-in-alt',
         ],
         [
-            'text' => 'multilevel',
-            'icon' => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                        ],
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text' => 'important',
-            'icon_color' => 'red',
-            'url' => '#',
-        ],
-        [
-            'text' => 'warning',
-            'icon_color' => 'yellow',
-            'url' => '#',
-        ],
-        [
-            'text' => 'information',
-            'icon_color' => 'cyan',
-            'url' => '#',
+            'text' => 'Register',
+            'url'  => 'register', // Dibuat oleh Della
+            'icon' => 'fas fa-fw fa-user-plus',
         ],
     ],
-
+    
     /*
     |--------------------------------------------------------------------------
     | Menu Filters
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Halaman tes untuk melihat tampilan Sidebar
 Route::get('/', function () {
     return view('welcome');
 });
